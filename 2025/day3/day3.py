afa0fbfe7d594bad392ec4efa1124b5d7bd2f21c
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-12-03 09:14:08 +0100

    2025 day 3 part 2

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -1,14 +1,15 @@
 from typing import List
 
-
-def get_max_joltage(bank: List[int]) -> int:
-    # get first digit:
-    first_digit = max(bank[:-1])
-    # first occurrence
-    first_occurrence = bank.index(first_digit)
-    # second digit
-    second_digit = max(bank[first_occurrence+1:])
-    return 10 * first_digit + second_digit
+def get_max_joltage(bank: List[int], batteries: int) -> int:
+    values = []
+    min_idx = 0
+    for i in range(batteries):
+        slack = batteries - len(values) - 1  # number of digits at end of bank to ignore for this round
+        next_digit = max(bank[min_idx:len(bank)-slack])
+        values.append(next_digit)
+        first_occurrence = min_idx + bank[min_idx:].index(next_digit)
+        min_idx = first_occurrence+1
+    return int(''.join(map(str, values)))
 
 
 if __name__ == '__main__':
@@ -18,4 +19,6 @@ if __name__ == '__main__':
             banks.append([int(c) for c in line.strip()])
 
     print("part 1")
-    print(sum([get_max_joltage(b) for b in banks]))
+    print(sum([get_max_joltage(b, 2) for b in banks]))
+    print("part 1")
+    print(sum([get_max_joltage(b,12) for b in banks]))
