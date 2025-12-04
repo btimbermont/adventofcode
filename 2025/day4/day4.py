e269ea2a0c0c3518f06115fc78a1630d71a6ad55
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-12-04 08:32:07 +0100

    2025 day 4 part 2

--- 2025/day4/day4.py
+++ 2025/day4/day4.py
@@ -1,12 +1,33 @@
-from advent_utils.two_d_utils import String2dMap
+from typing import List
 
-if __name__ == '__main__':
-    map = String2dMap('input.txt')
-    print("part 1")
+from advent_utils.two_d_utils import String2dMap, Point
+
+
+def get_accessible_rolls(map: String2dMap) -> List[Point]:
     access_rolls = []
     for roll in map.lookup_content('@'):
         neighbors = map.get_neighbors(roll, include_diagonals=True)
         neighboring_rolls = len([n for n in neighbors if map.get_cell(n) == '@'])
         if neighboring_rolls < 4:
             access_rolls.append(roll)
-    print(len(access_rolls))
+    return access_rolls
+
+
+if __name__ == '__main__':
+    map = String2dMap('input.txt')
+    print("part 1")
+    print(len(get_accessible_rolls(map)))
+
+    print("part 2")
+    removed_rolls = 0
+    rolls_to_remove = get_accessible_rolls(map)
+    while rolls_to_remove:
+        for roll in rolls_to_remove:
+            map.set_cell(roll, 'x')
+        removed_rolls += len(rolls_to_remove)
+        print(f'removed {len(rolls_to_remove)} rolls')
+        print(map)
+        for roll in rolls_to_remove:
+            map.set_cell(roll, '.')
+        rolls_to_remove = get_accessible_rolls(map)
+    print(f'Removed {removed_rolls} in total')
