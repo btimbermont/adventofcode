f1291868f7848aae868e5a300a030c98a3a1c470
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-12-05 09:24:26 +0100

    day 5 part 2

--- 2025/day5/day5.py
+++ 2025/day5/day5.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
                     ranges = False
             else:
                 ids_to_lookup.append(int(line))
-    fresh_ranges.sort(key=lambda x: x[0])
 
     print('part 1')
     fresh_ids = []
@@ -29,3 +28,22 @@ if __name__ == '__main__':
         if matching_range:
             fresh_ids.append(id)
     print(len(fresh_ids))
+
+    print('part 2')
+    fresh_ranges.sort(key=lambda x: x[0])
+    max_id = -1000
+    fresh_ids = 0
+    for range in fresh_ranges:
+        if max_id < range[0]:
+            fresh_ids += range[1] - range[0] + 1
+            max_id = range[1]
+        elif max_id > range[1]:
+            continue
+        elif in_range(max_id, range):
+            fresh_ids += range[1] - max_id
+            max_id = range[1]
+        else:
+            raise ValueError(f'something is off with tyour range checks! maxId={max_id} range={range}')
+        print(f'After range {range}: max_id {max_id} f_ids {fresh_ids}')
+    print(fresh_ids)
+
