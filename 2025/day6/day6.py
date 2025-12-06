92dcbedd2811321e87ced6484c21b08cacb3bb5d
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-12-06 13:33:06 +0100

    2025 day 6 part 2

--- 2025/day6/day6.py
+++ 2025/day6/day6.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 def process_problem(problem: List[int]) -> int:
-    values, operator = [int(v) for v in problem[:-1]] , problem[-1]
+    values, operator = [int(v) for v in problem[:-1]], problem[-1]
     if operator == '+':
         return sum(values)
     elif operator == '*':
@@ -13,6 +13,7 @@ def process_problem(problem: List[int]) -> int:
 
 
 if __name__ == '__main__':
+    print('part 1')
     lists = []
     with open('input.txt', 'r') as file:
         for line in file:
@@ -21,11 +22,31 @@ if __name__ == '__main__':
                 lists.append([])
             for i, value in enumerate(values):
                 lists[i].append(value)
-
-    print('part 1')
     total = 0
     for problem in lists:
         value = process_problem(problem)
-        print(f'{problem} = {value}')
         total += value
     print(f'Solution: {total}')
+
+    print('part 1')
+    lines = []
+    with open('input.txt', 'r') as file:
+        for line in file:
+            lines.append(line)
+    max_length = max([len(l) for l in lines])
+    values = []
+    solutions = []
+    for i in range(max_length - 1, -1, -1):
+        values.append('')
+        for line in lines:
+            if i >= len(line):
+                continue
+            if line[i] in '+*':
+                if line[i] == '+':
+                    solutions.append(sum([int(v) for v in values if len(v)]))
+                else:
+                    solutions.append(math.prod([int(v) for v in values if len(v)]))
+                values = []
+            elif line[i] in '0123456789':
+                values[-1] += line[i]
+    print(f'Solution: {sum(solutions)}')
