bfe99b2fc13093d3c0debe3d4911624040145536
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-12-08 23:57:57 +0100

    2025 day 8 part 2

--- 2025/day8/day8.py
+++ 2025/day8/day8.py
@@ -41,7 +41,32 @@ if __name__ == '__main__':
         if connections == 0:
             break
     all_circuits = list(set([tuple(circuit) for circuit in connected_junctions.values()]))
-    all_circuits.sort(key = lambda c: -len(c))
+    all_circuits.sort(key=lambda c: -len(c))
     circuit_sizes = [len(c) for c in all_circuits]
     print(circuit_sizes)
     print(math.prod(circuit_sizes[:3]))
+
+    print('part 2')
+    connected_junctions = dict()
+    for junction_a, junction_b, dist in full_distance_list:
+        circuit_a = connected_junctions.get(junction_a)
+        circuit_b = connected_junctions.get(junction_b)
+
+        if circuit_a is None and circuit_b is None:
+            new_circuit = [junction_a, junction_b]
+            connected_junctions[junction_a] = new_circuit
+            connected_junctions[junction_b] = new_circuit
+        elif circuit_a is None:
+            circuit_b.append(junction_a)
+            connected_junctions[junction_a] = circuit_b
+        elif circuit_b is None:
+            circuit_a.append(junction_b)
+            connected_junctions[junction_b] = circuit_a
+        elif circuit_a != circuit_b:
+            circuit_a.extend(circuit_b)
+            for junction in circuit_b:
+                connected_junctions[junction] = circuit_a
+        if len(connected_junctions[junction_a]) == len(points):
+            break
+    print(f'last connection = {junction_a, junction_b}')
+    print(junction_a[0] * junction_b[0])
