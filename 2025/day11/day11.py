cb941e333e128e9090fb4adcd6835d0c19400214
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-12-11 11:34:21 +0100

    2025 day 11 part 2

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -6,6 +6,7 @@ from typing import List
 class Node:
     name: str
     paths: List['Node']
+    reachable: List[str] = None
 
     def __repr__(self):
         return f'Node({self.name}, {[p.name for p in self.paths]})'
@@ -13,10 +14,12 @@ class Node:
     def __str__(self):
         return self.__repr__()
 
-    def paths_to(self, destination: str) -> int:
+    def paths_to(self, destination: str, forbidden_nodes: List[str] = []) -> int:
         if self.name == destination:
             return 1
-        return sum([p.paths_to(destination) for p in self.paths])
+        if destination not in self.reachable:
+            return 0
+        return sum([p.paths_to(destination, forbidden_nodes) for p in self.paths if p.name not in forbidden_nodes])
 
 
 def parse_graph(input: str):
@@ -32,11 +35,50 @@ def parse_graph(input: str):
                 if p not in nodes.keys():
                     nodes[p] = Node(p, [])
                 nodes[name].paths.append(nodes[p])
+    # have each node list its reachable paths
+    for n in nodes.values():
+        update_reachable(n)
     return nodes
 
 
+def update_reachable(node: Node):
+    if node.reachable is None:
+        reachable = set()
+        reachable.add(node.name)
+        for p in node.paths:
+            update_reachable(p)
+            reachable.update(p.reachable)
+        node.reachable = reachable
+
+
 if __name__ == '__main__':
-    nodes = parse_graph('input.txt')
-    print(nodes)
     print('part 1')
+    nodes = parse_graph('input.txt')
     print(nodes['you'].paths_to('out'))
+
+    print('part 2')
+    nodes = parse_graph('input.txt')
+    result = 0
+    if 'dac' in nodes['fft'].reachable:
+        # fft > dac
+        print('a...')
+        a = nodes['svr'].paths_to('fft', ['dac'])
+        print('b...')
+        b = nodes['fft'].paths_to('dac')
+        print('c...')
+        c = nodes['dac'].paths_to('out')
+        result += a * b * c
+    else:
+        print("Can't do fft > dac")
+    if 'fft' in nodes['dac'].reachable:
+        # dac > fft
+        print('a...')
+        a = nodes['svr'].paths_to('dac', ['fft'])
+        print('b...')
+        b = nodes['dac'].paths_to('fft')
+        print('c...')
+        c = nodes['fft'].paths_to('out')
+        result += a * b * c
+    else:
+        print("Can't do dac > fft")
+    print(result)
